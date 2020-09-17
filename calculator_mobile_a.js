40403c35f3a2ed097aed9c9259e6a3bf906e3fc7
taiminghuang <66934068+taiminghuang@users.noreply.github.com>
2020-09-17 21:46:45 +0800

    Update calculator_mobile_a.js

--- calculator_mobile_a.js
+++ calculator_mobile_a.js
@@ -7874,9 +7874,14 @@ function m_ln(x){
  var sum_tb=0;
  var sum_tt=0;
  var sum_ta_0=0 ;
+ var ans_t = 0;  
           var data_trans = MTX_COL;
            if( MTX_COL >=100){       
                 MTX_COL=100 ;}
+       var xx_fst=xx.substr(0,1); 
+      if ( xx == 0){ var ans_t = message_1(13) ;  return ans_t;} 
+      if (xx_fst == "-") {  var ans_t = message_1(5); return ans_t;} 
+      if ((xx == "1" || xx == 1) && xx.length==1 ) {  var ans_t = 0 ; return ans_t;}
        var xx_str =""; 
        var xx_str = m_s_e_ln(xx,0);       
        var nub_e =  m_s_e_ln(xx,1); 
